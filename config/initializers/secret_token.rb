0fc665619140a6acbfdd7a0000d0a51ae78e5c05
Basab K. Maulik <bkm.hadoop@gmail.com>
2010-12-11 14:23:01 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '970ff662dffbd1001360e7c125523fd321ff81649c44b3eda2b4e98ce245c4baa0d64fbfc14a2973344fc1c7804ddba8d21956cd7906e8083a2d805ca3aea919'
