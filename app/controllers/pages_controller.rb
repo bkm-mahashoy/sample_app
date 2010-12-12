f3b57fb757e500312ee7d11aa7c34e8cf32cc92b
Basab K. Maulik <bkm.hadoop@gmail.com>
2010-12-11 17:43:15 -0800

    Finished preliminary (most static) pages. Pages have been tested with rspec.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,13 @@
 class PagesController < ApplicationController
   def home
+    @page_title = 'Home'
   end
 
   def contact
+    @page_title = 'Contact'
   end
 
+  def about
+    @page_title = 'About'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SampleApp::Application.routes.draw do
   get "pages/home"
   get "pages/contact"
+  get "pages/about"
 
   # match ':controller(/:action(/:id(.:format)))'
 end
